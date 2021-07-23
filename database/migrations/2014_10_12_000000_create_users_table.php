1814fd293a56374b37663badd997a715367773f8
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-07-23 13:40:28 +0700

    add spatie (middleware permission)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // you can add member/customer table one to one relation to this table
+        // if user is an memebr/cutomer, the user must have record in the member/cutomer table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/seeders/PermissionRoleUserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionRoleUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        // Permission::create(['name' => 'edit articles']);
+        // Permission::create(['name' => 'delete articles']);
+        // Permission::create(['name' => 'publish articles']);
+        // Permission::create(['name' => 'unpublish articles']);
+
+        // create roles and assign existing permissions
+        // $role1 = Role::create(['name' => 'writer']);
+        // $role1->givePermissionTo('edit articles');
+        // $role1->givePermissionTo('delete articles');
+
+        // $role2 = Role::create(['name' => 'admin']);
+        // $role2->givePermissionTo('publish articles');
+        // $role2->givePermissionTo('unpublish articles');
+
+        $role3 = Role::create(['name' => 'Super-Admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+        $user = \App\Models\User::factory()->create([
+            'name' => 'SuperAdmin',
+            'username' => 'superadmin',
+            'email' => 'superadmin@example.com',
+        ]);
+        $user->assignRole($role3);
+
+        // In login method check the loged-in user Role
+    }
+}
