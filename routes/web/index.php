34024656836871af27af40d76834bfb36fa5eac4
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-07-23 14:01:14 +0700

    change route file works flow

---
+++ routes/web/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// supaya rapi
+// add some folder to group the route by Role
+// copy file index.php to inside each new created folder from admin/index.php
+
+$dir   = base_path('routes/web');
+
+require $dir . '/admin/index.php';
