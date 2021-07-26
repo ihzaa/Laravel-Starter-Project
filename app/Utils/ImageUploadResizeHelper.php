6ddb356a248329b866b8288cd73f4a20ec122bee
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-07-26 08:02:28 +0700

    update comment in ImageIploadResizeHelper class

--- app/Utils/ImageUploadResizeHelper.php
+++ app/Utils/ImageUploadResizeHelper.php
@@ -2,9 +2,34 @@
 
 namespace App\Utils;
 
+
+/**
+ * Upload Image and Resize (if needed)
+ *
+ * This class can used for uploading image dan resize the size (optional) with keep the aspec ratio.
+ *
+ * @copyright  2021 Ihza
+ * @license
+ * @version     1.0
+ * @link
+ * @since      Class available since Release 1.0
+ */
 class ImageUploadResizeHelper
 {
-    public static function resizeImageAndUpload($imageRequestObject, $requiredSize, $path, $name, $generateThumbnail = true)
+    /**
+     * Upload the image
+     *
+     * @param Illuminate\Http\Request   $imageRequestObject  the image object from request object
+     * @param integer $requiredSize the max size for uploaded image. if don't want to resize the image, just give this false
+     * @param string $path where the image will be placed
+     * @param string $name the name of uploaded image
+     * @param boolean $generateThumbnail condition to generate the thumbnail or not
+     *
+     * @throws None
+     * @author Ihza
+     * @return array contain full path the uploaded image
+     */
+    public static function upload($imageRequestObject, $requiredSize, $path, $name, $generateThumbnail = true)
     {
         $image = Image::make($imageRequestObject);
         $ext = $image->getClientOriginalExtension();
@@ -12,7 +37,7 @@ class ImageUploadResizeHelper
         $height = $image->height();
 
         // Check if image resize is required or not
-        if ($requiredSize >= $width && $requiredSize >= $height) {
+        if ($requiredSize >= $width && $requiredSize >= $height || $requiredSize == false) {
             $image->save(public_path($path) . $name . '.' . $ext);
             $data['resize'] = false;
         } else {
