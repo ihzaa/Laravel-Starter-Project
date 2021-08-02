0d1f3faf40df58322fce0d0620e6e1424db53047
ihzaahmad0 <ihzaahmad0@gmail.com>
2021-08-02 19:30:31 +0700

    rollback route

--- routes/web/admin/example.php
+++ routes/web/admin/example.php
@@ -4,12 +4,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('tes', function () {
-    // iniTestingAja();
-    $usr = new User;
-    $usr->name = '123';
-    $usr->username = '123';
-    $usr->save();
-    return $usr;
+    iniTestingAja();
 });
 
 // INI ADALAH CONTOH FILE ROUTE
