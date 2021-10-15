d8227c42e263e55b82680ae2ad90845d4e65163d
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2021-10-15 12:25:14 +0700

    add user stamp blade

---
+++ resources/views/layouts/general_informations/userResponsibleStamp.blade.php
@@ -0,0 +1,42 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                Aktifitas Bersangkutan
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Aktivitas</th>
+                            <th>Nama User</th>
+                            <th>Waktu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Dibuat Oleh</td>
+                            <td>{{ optional($data->createdByUser)->name }}</td>
+                            <td>{{ $data->created_at }}</td>
+                        </tr>
+                        <tr>
+                            <td>Diupdate Oleh</td>
+                            <td>{{ optional($data->updatedByUser)->name }}</td>
+                            <td>{{ $data->updated_at }}</td>
+                        </tr>
+                        <tr>
+                            <td>Dihapus Oleh</td>
+                            <td>{{ optional($data->deletedByUser)->name }}</td>
+                            <td>{{ $data->deleted_at }}</td>
+                        </tr>
+                        <tr>
+                            <td>Direstore Oleh</td>
+                            <td>{{ optional($data->restoredByUser)->name }}</td>
+                            <td>{{ $data->restored_at }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
