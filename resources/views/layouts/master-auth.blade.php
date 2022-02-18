b2ba598a1d1da3647d81a5fff5303b7f2679210f
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-02-18 11:05:16 +0700

    add feature reset password

--- resources/views/layouts/master-auth.blade.php
+++ resources/views/layouts/master-auth.blade.php
@@ -5,18 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>{{ config('app.name') }} | Login</title>
+    <title>{{ config('app.name') }} | @yield('page-title')</title>
 
-    <meta name="description"
-        content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta name="description" content="Halaman @yield('page-title')">
     <meta name="author" content="pixelcave">
     <meta name="robots" content="noindex, nofollow">
 
     <!-- Open Graph Meta -->
-    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">
+    <meta property="og:title" content="">
     <meta property="og:site_name" content="OneUI">
-    <meta property="og:description"
-        content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta property="og:description" content="Halaman @yield('page-title')">
     <meta property="og:type" content="website">
     <meta property="og:url" content="">
     <meta property="og:image" content="">
@@ -86,80 +84,17 @@
             <!-- Page Content -->
             <div class="hero-static d-flex align-items-center">
                 <div class="w-100">
-                    <!-- Sign In Section -->
+                    <!-- Reminder Section -->
                     <div class="bg-white">
                         <div class="content content-full">
-                            <div class="row justify-content-center">
-                                <div class="col-md-8 col-lg-6 col-xl-4 py-4">
-                                    <!-- Header -->
-                                    <div class="text-center">
-                                        <p class="mb-2">
-                                            <i class="fa fa-2x fa-circle-notch text-primary"></i>
-                                        </p>
-                                        <h1 class="h4 mb-1">
-                                            Sign In
-                                        </h1>
-                                        {{-- <h2 class="h6 font-w400 text-muted mb-3">
-                                            A perfect match for your project
-                                        </h2> --}}
-                                    </div>
-                                    <!-- END Header -->
-
-                                    <!-- Sign In Form -->
-                                    <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _js/pages/op_auth_signin.js) -->
-                                    <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
-                                    <form class="js-validation-signin" action="{{ route('auth.login.post') }}"
-                                        method="POST">
-                                        @csrf
-                                        <div class="py-3">
-                                            <div class="form-group">
-                                                <input type="text" class="form-control form-control-lg form-control-alt"
-                                                    id="login-username" name="username"
-                                                    placeholder="Username atau Email">
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password"
-                                                    class="form-control form-control-lg form-control-alt"
-                                                    id="login-password" name="password" placeholder="Password">
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="d-md-flex align-items-md-center justify-content-md-between">
-                                                    <div class="custom-control custom-switch">
-                                                        <input type="checkbox" class="custom-control-input"
-                                                            id="login-remember" name="remember">
-                                                        <label class="custom-control-label font-w400"
-                                                            for="login-remember">Remember Me</label>
-                                                    </div>
-                                                    {{-- <div class="py-2">
-                                                        <a class="font-size-sm font-w500"
-                                                            href="op_auth_reminder2.html">Forgot Password?</a>
-                                                    </div> --}}
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row justify-content-center mb-3">
-                                            <div class="col-md-6 col-xl-5">
-                                                <button type="submit" class="btn btn-block btn-primary">
-                                                    <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row justify-content-center mb-0 text-center">
-                                            <div class="col-md-6 col-xl-5">
-                                                <a href="{{ route('forget.password.index') }}">Lupa password?</a>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <!-- END Sign In Form -->
-                                </div>
-                            </div>
+                            @yield('content')
                         </div>
                     </div>
-                    <!-- END Sign In Section -->
+                    <!-- END Reminder Section -->
 
                     <!-- Footer -->
                     <div class="font-size-sm text-center text-muted py-3">
-                        <strong>{{ config('app.name') }} </strong> &copy; <span data-toggle="year-copy"></span>
+                        <strong>{{ config('app.name') }}</span>
                     </div>
                     <!-- END Footer -->
                 </div>
@@ -195,6 +130,13 @@
             webpack is putting everything together at assets/_js/main/app.js
         -->
     <script src="{{ asset('oneUI') }}/js/oneui.app.min.js"></script>
+
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('oneUI') }}/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{ asset('oneUI') }}/js/pages/op_auth_reminder.min.js"></script>
+
     @include('layouts.alerts.sweetalert')
 </body>
 
