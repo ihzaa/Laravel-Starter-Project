ef3c25399a96615885aebf67e37496b01934016d
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-02-22 10:43:46 +0700

    fix alert

--- resources/views/layouts/alerts/alert.blade.php
+++ resources/views/layouts/alerts/alert.blade.php
@@ -1,3 +1,38 @@
+<script>
+    const Toast = {
+        success(title = " ", message = " ") {
+            toastr["success"](`${message}`, `${title}`)
+        },
+        info(title = " ", message = " ") {
+            toastr["info"](`${message}`, `${title}`)
+        },
+        warning(title = " ", message = " ") {
+            toastr["warning"](`${message}`, `${title}`)
+        },
+        error(title = " ", message = " ") {
+            toastr["error"](`${message}`, `${title}`)
+        },
+    };
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": 0,
+        "extendedTimeOut": 0,
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut",
+        "tapToDismiss": false
+    }
+</script>
 @if ($errors->any())
     <script>
         Toast.error('{{ $errors->first() }}')
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -145,42 +145,6 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
 
-    <script>
-        const Toast = {
-            success(title = " ", message = " ") {
-                toastr["success"](`${message}`, `${title}`)
-            },
-            info(title = " ", message = " ") {
-                toastr["info"](`${message}`, `${title}`)
-            },
-            warning(title = " ", message = " ") {
-                toastr["warning"](`${message}`, `${title}`)
-            },
-            error(title = " ", message = " ") {
-                toastr["error"](`${message}`, `${title}`)
-            },
-        };
-
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": 0,
-            "extendedTimeOut": 0,
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut",
-            "tapToDismiss": false
-        }
-    </script>
-    @include('layouts.alerts.sweetalert')
     @include('layouts.alerts.alert')
     @include('layouts.alerts.input-invalid')
     @include('layouts.parts.loader')
